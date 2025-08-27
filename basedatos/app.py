0461fad1e63c76f439972a16a2e51fd365788bda
natalia <nataliamelendez2426@gmail.com>
2025-08-27 16:20:38 -0500

    Auto-sync: file changes

--- basedatos/app.py
+++ basedatos/app.py
@@ -1,4 +1,4 @@
-from flask import Flask , request, render_template, redirect, url_for, flash
+from flask import Flask , Flask, render_template, request, redirect, url_for, flash, session
 from flask_mysqldb import MySQL
 
 
@@ -38,5 +38,46 @@ def register():
             flash("✅ Registro exitoso, ya puedes iniciar sesión", "success")
             return redirect(url_for('login'))
 
-    return render_template("register.html")
+    return render_template("registro.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        correo = request.form['correo']
+        contrasena = request.form['contrasena']
+
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM Usuario WHERE Correo = %s AND Contraseña = %s', (correo, contrasena))
+        user = cursor.fetchone()
+
+        if user:
+            session['loggedin'] = True
+            session['id_usuario'] = user['ID_Usuario']
+            session['nombre'] = user['Nombre']
+            flash("✅ Bienvenido " + user['Nombre'], "success")
+            return redirect(url_for('dashboard'))
+        else:
+            flash("❌ Correo o contraseña incorrectos", "danger")
+
+    return render_template("login.html")
+
+@app.route('/forgot_password', methods=['GET', 'POST'])
+def forgot_password():
+    if request.method == 'POST':
+        correo = request.form['correo']
+        nueva_contrasena = request.form['nueva_contrasena']
+
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM Usuario WHERE Correo = %s", (correo,))
+        user = cursor.fetchone()
+
+        if user:
+            cursor.execute("UPDATE Usuario SET Contraseña = %s WHERE Correo = %s", (nueva_contrasena, correo))
+            mysql.connection.commit()
+            flash("✅ Contraseña actualizada, ahora inicia sesión", "success")
+            return redirect(url_for('login'))
+        else:
+            flash("⚠️ El correo no está registrado", "danger")
+
+    return render_template("olvidaste_contraseña.html")
 
