f67aabf693a2c6bac64b3de498d828f82bfe2bb6
natalia <nataliamelendez2426@gmail.com>
2025-08-27 15:47:34 -0500

    Auto-sync: file changes

--- basedatos/db.py
+++ basedatos/db.py
@@ -2,5 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return'hello word'
+
+
+@app.route('/username')
+def username():
+    return 'username'
+
+
+@app.route('/password')
+def password():
+    return 'password'
+
 if __name__ == '__main__':
     app.run(port = 3000, debug=True)
