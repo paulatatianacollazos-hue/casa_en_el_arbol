aeb54b973e63bd0195b3b17e0626f3977af649ab
natalia <nataliamelendez2426@gmail.com>
2025-09-04 13:25:07 -0500

    Auto-sync: file changes

--- app.py
+++ app.py
@@ -228,4 +228,5 @@ if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
+
