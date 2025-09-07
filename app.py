3bbc1191f18874e9bad3b54e6b87aaec277737f8
natalia <nataliamelendez2426@gmail.com>
2025-09-06 19:18:26 -0500

    Auto-sync: file changes

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'tu_correo@gmail.com'       # ⚡ pon tu correo
-app.config['MAIL_PASSWORD'] = 'tu_clave_app_google'       # ⚡ clave de aplicación Gmail
+app.config['MAIL_USERNAME'] = 'nataliamelendez2426@gmail.com'       # ⚡ pon tu correo
+app.config['MAIL_PASSWORD'] = 'tipz wgml ugeu dxnu'       # ⚡ clave de aplicación Gmail
 app.config['MAIL_DEFAULT_SENDER'] = ('Soporte Tienda', app.config['MAIL_USERNAME'])
 
 mail = Mail(app)
