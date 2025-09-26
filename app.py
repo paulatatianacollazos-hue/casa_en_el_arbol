6e11c117a35b7416649480176c81443a4e518cd6
natalia <nataliamelendez2426@gmail.com>
2025-09-25 19:03:20 -0500

    Auto-sync: file changes

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+import os
+import re
+from datetime import datetime, timedelta
+
 from flask import Flask, render_template
 from flask_mail import Mail
 from itsdangerous import URLSafeTimedSerializer
+from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 from sqlalchemy.exc import SQLAlchemyError
 from flask_login import (
@@ -14,19 +19,18 @@ from basedatos.models import db, Usuario
 # ------------------ BLUEPRINTS ------------------ #
 from routes.cliente import cliente
 from routes.administrador import admin
-
+from routes.auth import auth  # <--- NUEVO para login, registro y logout
 
 # ------------------ CONFIG ------------------ #
 app = Flask(__name__)
 
-# 🔑 Configuración general
 app.config['SECRET_KEY'] = "mi_clave_super_secreta_y_unica"
 DB_URL = 'mysql+pymysql://root:2426@127.0.0.1:3306/Tienda_db'
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_pre_ping': True}
 
-# 📧 Configuración de correo
+# Configuración de correo
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
@@ -35,25 +39,23 @@ app.config['MAIL_USERNAME'] = 'casaenelarbol236@gmail.com'
 app.config['MAIL_PASSWORD'] = 'usygdligtlewedju'
 app.config['MAIL_DEFAULT_SENDER'] = ('Casa en arbol', app.config['MAIL_USERNAME'])
 
-# Inicializar extensiones
 mail = Mail(app)
 s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 db.init_app(app)
 
 # ------------------ FLASK-LOGIN ------------------ #
 login_manager = LoginManager()
-login_manager.login_view = "login"
+login_manager.login_view = "auth.login"  # <--- MUY IMPORTANTE, apunta a la ruta de login del blueprint auth
 login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    """Carga el usuario actual por ID para Flask-Login"""
     return Usuario.query.get(int(user_id))
 
-# ------------------ BLUEPRINTS ------------------ #
+# ------------------ REGISTRO DE BLUEPRINTS ------------------ #
 app.register_blueprint(cliente, url_prefix="/cliente")
 app.register_blueprint(admin, url_prefix="/admin")
-
+app.register_blueprint(auth, url_prefix="/auth")
 
 # ------------------ RUTAS PÚBLICAS ------------------ #
 @app.route('/')
