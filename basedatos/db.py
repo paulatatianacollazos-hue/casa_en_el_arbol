6729c10250e82572f183cab3aaae51af1b01ee96
paula collazos <paulatatianacollazos@gmail.com>
2025-09-28 22:42:29 -0500

    Auto-sync: file changes

--- basedatos/db.py
+++ basedatos/db.py
@@ -1,11 +1,11 @@
-# basedatos/db.py
+# basedatos/db.py 2426
 import mysql.connector
 
 def get_connection():
     """Devuelve una conexión nueva a la base de datos."""
     return mysql.connector.connect(
         user='root',
-        password='2426',
+        password='paula123',
         host='localhost',
         database='tienda_db',
     )
