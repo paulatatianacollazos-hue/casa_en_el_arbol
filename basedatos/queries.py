7af782c917be8891edbfbf2e5deb500c379d0a74
paula collazos <paulatatianacollazos@gmail.com>
2025-09-29 10:23:51 -0500

    Auto-sync: file changes

--- basedatos/queries.py
+++ basedatos/queries.py
@@ -664,7 +664,7 @@ def registrar_pedido(nombre_comprador, fecha_entrega, hora_entrega, destino, usu
         db.session.rollback()
         return {"success": False, "message": str(e)}
     
-from basedatos import obtener_conexion
+
 
 def obtener_pedidos_por_cliente(id_cliente):
     conexion = get_connection()
