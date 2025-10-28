42f3a9ce8b5f22cb31e7e1e741f5f80e97afd4c1
paula collazos <paulatatianacollazos@gmail.com>
2025-10-28 16:59:38 -0500

    Auto-sync: file changes

--- routes/Empleado/routers.py
+++ routes/Empleado/routers.py
@@ -4,7 +4,5 @@ empleado = Blueprint('empleado', __name__)
 
 
 @empleado.route('/dashboard')
-@empleado.route('/instalador')
-@empleado.route('/transportista')
 def instalador_dashboard():
     return render_template('dashboards/dashboard.html')
