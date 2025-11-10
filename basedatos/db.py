f1528302311a0af89b9b3c33516440560f46b125
David Alape <david2008alejo@gmail.com>
2025-11-10 00:01:27 -0500

    Auto-sync: file changes

--- basedatos/db.py
+++ basedatos/db.py
@@ -6,7 +6,7 @@ def get_connection():
     """Devuelve una conexión nueva a la base de datos."""
     return mysql.connector.connect(
         user='root',
-        password='paula123',
+        password='',
         host='localhost',
         database='tienda_db',
     )
