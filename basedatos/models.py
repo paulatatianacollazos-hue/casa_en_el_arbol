f5757924b09352887b794786fe8caaecf13c8259
David Alape <david2008alejo@gmail.com>
2025-12-24 01:30:31 -0500

    Auto-sync: file changes

--- basedatos/models.py
+++ basedatos/models.py
@@ -23,7 +23,7 @@ class Usuario(UserMixin, db.Model):
 
     # Relaciones
     calendarios = db.relationship('Calendario', back_populates='usuario', lazy=True)
-    notificaciones = db.relationship('Notificaciones', backref='usuario',
+    notificaciones = db.relationship('Notificaciones', back_populates='usuario',
                                      lazy=True)
     novedades = db.relationship('Novedades', backref='usuario', lazy=True)
     pedidos = db.relationship('Pedido', backref='usuario', lazy=True,
